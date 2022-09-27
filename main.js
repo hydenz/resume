be324e35f9dac4ce0d303ce677c98eb75e428527
Marcelo F. de Lima <marcelof_lima@hotmail.com>
2022-09-27 11:46:08 -0300

    refactor: code style

--- main.js
+++ main.js
@@ -2,29 +2,29 @@ import { jsPDF } from 'jspdf';
 import translations from './translations.json';
 
 function changeLanguage(language) {
-  const elements = Array.from(document.querySelectorAll("[data-translate]"));
+  const elements = Array.from(document.querySelectorAll('[data-translate]'));
 
   elements.forEach((element) => {
-    const translationKey = element.getAttribute("data-translate");
+    const translationKey = element.getAttribute('data-translate');
     element.innerText = translations[translationKey][language];
   });
 }
 
-const languageSelect = document.querySelector("select");
+const languageSelect = document.querySelector('select');
 
-languageSelect.addEventListener("change", (e) =>
+languageSelect.addEventListener('change', (e) =>
   changeLanguage(e.target.value)
 );
 
 const filename = {
-  "en-US": "Marcelo Lima's Resume.pdf",
-  "pt-BR": "Marcelo Lima.pdf",
+  'en-US': "Marcelo Lima's Resume.pdf",
+  'pt-BR': 'Marcelo Lima.pdf',
 };
 
-document.querySelector("button").addEventListener("click", () => {
-  const doc = new jsPDF({ unit: "px" });
-  doc.setFont("Ubuntu", "normal");
-  doc.html(document.querySelector(".resume"), {
+document.querySelector('button').addEventListener('click', () => {
+  const doc = new jsPDF({ unit: 'px' });
+  doc.setFont('Ubuntu', 'normal');
+  doc.html(document.querySelector('.resume'), {
     width: 1080,
     windowWidth: 1899,
     callback: (doc) => doc.save(filename[languageSelect.value]),
