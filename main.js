dc541675f9905359f120e0584cdd48b73d9da7da
Marcelo F. de Lima <marcelof_lima@hotmail.com>
2023-10-17 21:04:25 -0300

    feat: add language selection by url params

--- main.js
+++ main.js
@@ -1,37 +1,44 @@
 import { jsPDF } from 'jspdf';
 import translations from './translations.json';
 
-function changeLanguage(language) {
-  const elements = Array.from(document.querySelectorAll('[data-translate]'));
-
-  elements.forEach((element) => {
-    const translationKey = element.getAttribute('data-translate');
-    element.innerText = translations[translationKey][language];
-  });
-}
-
+const LANGUAGES = ["pt-BR", "en-US"];
 const languageSelect = document.querySelector('select');
-
-languageSelect.addEventListener('change', (e) =>
-  changeLanguage(e.target.value)
-);
-
+const searchParams = new URLSearchParams(window.location.search);
 const filename = {
   'en-US': "Marcelo Lima's Resume.pdf",
   'pt-BR': 'Marcelo Lima.pdf',
 };
 
+function changeLanguage(language) {
+  const elements = document.querySelectorAll('[data-translate]');
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute('data-translate');
+    element.innerText = translations[translationKey][language];
+  });
+
+  searchParams.set('language', language);
+  const newSearchString = searchParams.toString();
+  const [baseUrl] = window.location.href.split('?');
+  window.history.replaceState(null, '', `${baseUrl}?${newSearchString}`);
+}
+
+languageSelect.addEventListener('change', (e) => changeLanguage(e.target.value));
+
+const language = searchParams.get('language');
+if (LANGUAGES.includes(language)) {
+  languageSelect.value = language;
+  changeLanguage(language);
+}
+
 document.querySelector('button').addEventListener('click', () => {
   const doc = new jsPDF({ unit: 'px' });
   doc.setFont('Ubuntu', 'normal');
   doc.html(document.querySelector('.resume'), {
-    width: 1080,
-    windowWidth: 1899,
+    width: 950,
+    windowWidth: 1903,
     callback: (doc) => doc.save(filename[languageSelect.value]),
     html2canvas: {
-      width: 1080,
-      windowWidth: 1899,
-      x: 552,
+      x: 500,
     },
   });
 });
